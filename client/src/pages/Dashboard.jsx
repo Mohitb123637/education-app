84971904257e0c0edb1c9ee878cd011e4f4ab4c3
mohit <mohitb12363@gamil.com>
2024-07-19 11:52:38 +0530

    complete sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSideBar from '../components/DashSideBar';
+import { DashProfile } from '../components/DashProfile';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* sidebar */}
+      <div className=" md:w-56">
+        <DashSideBar />
+      </div>
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 };
 
 export default Dashboard;
